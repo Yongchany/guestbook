a7a910fde4c56f5750098b6e3261dfa8f1cfead9
Yongchany <jeffrey0301@naver.com>
2023-05-16 15:30:54 +0900

    0509

--- src/main/java/kr/ac/pool/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/ac/pool/guestbook/controller/GuestBookController.java
@@ -1,18 +1,30 @@
 package kr.ac.pool.guestbook.controller;
 
+import kr.ac.pool.guestbook.dto.PageRequestDTO;
+import kr.ac.pool.guestbook.dto.PageResultDTO;
+import kr.ac.pool.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestBookController {
+    private final GuestbookService service;
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
 
     @GetMapping({"/", "/list"})
-    public String list(){
-        log.info("방명록 목록 화면");
-        return "/guestbook/list";
+    public String list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("페이지 요청 정보" + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
--- src/main/java/kr/ac/pool/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/kr/ac/pool/guestbook/service/GuestbookServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.function.Function;
 
 @Service
